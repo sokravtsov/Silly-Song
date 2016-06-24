d5e699cf265fd14a9c65510db1b1970bfe7250db
sokravtsov <kravtsov@happimess.ru>
2016-06-25 00:10:41 +0300

    unzip file

---
+++ Silly Song/Silly Song/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  Silly Song
+//
+//  Created by Sergey Kravtsov on 08.06.16.
+//  Copyright © 2016 Sergey Kravtsov. All rights reserved.
+//
+
+import UIKit
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joinWithSeparator("\n")
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    
+    @IBAction func reset(sender: AnyObject) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
+    
+    @IBAction func displayLyrics(sender: AnyObject) {
+        if nameField.text == nil || nameField.text == "" {
+            return
+        } else {
+            lyricsView.text = lyricsForName(bananaFanaTemplate, fullName: nameField.text!)
+        }
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+func shortNameFromName(name: String) -> String {
+    let lowercaseName = name.lowercaseString
+    let vowelSet = NSCharacterSet(charactersInString: "aeiou")
+    if let firstVowelRange = lowercaseName.stringByFoldingWithOptions(.DiacriticInsensitiveSearch, locale: nil).rangeOfCharacterFromSet(vowelSet) {
+        return lowercaseName.substringFromIndex(firstVowelRange.startIndex)
+    }
+    return lowercaseName
+}
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    let shortName = shortNameFromName(fullName)
+    
+    let lyrics = lyricsTemplate
+        .stringByReplacingOccurrencesOfString("<FULL_NAME>", withString: fullName)
+        .stringByReplacingOccurrencesOfString("<SHORT_NAME>", withString: shortName)
+    return lyrics
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
